2c82b8c43d57bfb4dd1172bc60bbfc77a80dd06c
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2019-11-16 20:09:25 +0700

    first functional json merger function

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const merge = require('lodash.merge')
+
 exports.jsonMerger = (req, res) => {
-    res.send("hello")
+    const { object1: rawObject1, object2: rawObject2 } = req.body
+
+    if (typeof rawObject1 === 'string' && typeof rawObject2 === 'string') {
+        try {
+            const o1 = JSON.parse(rawObject1)
+            const o2 = JSON.parse(rawObject2)
+
+            res.send(merge(o1, o2))
+        } catch(err) {
+            res.send(err)
+        }
+    }
 }
